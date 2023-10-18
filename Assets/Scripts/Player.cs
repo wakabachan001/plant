f17ca6c96dea381986c5160abbb390eb34c5506f
白鳥謙信 <h1202315001@hiratagakuen.onmicrosoft.com>
2023-10-18 11:58:46 +0900

    スクリプト　player　と　Controller2D　を追加

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+
+
+    Controller2D controller;
+
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+    }
+}
